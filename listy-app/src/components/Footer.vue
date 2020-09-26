e5fb4cdfe156a003f0d38af27b593a8650c38c4d
Francisco <frbernad@itba.edu.ar>
2020-09-25 21:41:00 -0300

    added footer

---
+++ listy-app/src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-footer app>
+    <v-container fluid>
+          <p class="text-center">Copyright © 2020 Listy</p>
+    </v-container>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    name: "Footer"
+  }
+</script>
+
+<style scoped>
+
+</style>
