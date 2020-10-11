cbdc7fc1b12512bb03b6239f8506ff7dbd7e3c78
Micky <mconca@itba.edu.ar>
2020-10-11 13:11:44 -0300

    add component listbar

--- listy-app/src/views/MisListas.vue
+++ listy-app/src/views/MisListas.vue
@@ -1,10 +1,32 @@
 <template>
-
+  <v-container>
+    <v-container fluid>
+      <v-row justify="center">
+        <v-col cols="12" sm="9" md="7" lg="6">
+          <div class="d-flex justify-center">
+            <v-btn color="primary" x-large to="/UserHome/createList">Crear Lista</v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+    <ListBar class="my-5" :title="Title"></ListBar>
+    <ListBar class="my-5"></ListBar>
+    <ListBar class="my-5"></ListBar>
+  </v-container>
 </template>
 
 <script>
+import ListBar from '../components/ListBar'
 export default {
-  name: "MisListas"
+  name: "MisListas",
+  components: {
+    ListBar
+  },
+  data () {
+    return {
+      Title: 'compra semanal'
+    }
+  }
 }
 </script>
 
