4bb4cf834d7da867713158e8697a8462bfaf9cdf
Francisco <frbernad@itba.edu.ar>
2020-10-20 22:23:12 -0300

    added rating system

--- listy-app/src/components/AppBar.vue
+++ listy-app/src/components/AppBar.vue
@@ -1,21 +1,22 @@
 <template>
   <v-app-bar app clipped-left dense>
-    <v-row>
-
-    <v-app-bar-nav-icon>
-    </v-app-bar-nav-icon>
-      <router-link to="/UserHome">
-        <v-img src="../assets/images/longlogonobg.png" contain max-width="220" max-height="50">
-        </v-img>
-      </router-link>
-      <v-btn @click="logout">
-        logout
-      </v-btn>
+    <v-row align="center">
+      <v-col cols="4" class="d-flex justify-start">
+        <router-link to="/home">
+          <v-img src="../assets/images/longlogonobg.png" contain max-width="70" max-height="35">
+          </v-img>
+        </router-link>
+      </v-col>
+      <v-col cols="6" class="offset-2 d-flex justify-end">
+        <v-btn @click="logout" class="mx-4">
+          logout
+        </v-btn>
         <v-btn>
           <v-spacer></v-spacer>
-            <v-icon class="px-1">mdi-account</v-icon>
-            JOE
+          <v-icon class="px-1">mdi-account</v-icon>
+          {{user}}
         </v-btn>
+      </v-col>
     </v-row>
 
   </v-app-bar>
@@ -24,12 +25,17 @@
 <script>
   export default {
     name: "AppBar",
-    methods:{
-      async logout(){
+    computed:{
+      user(){
+        return this.$store.getters["user/username"];
+      }
+    },
+    methods: {
+      async logout() {
         try {
-        await this.$store.dispatch('logout');
-        await this.$router.replace('/');
-        }catch(e){
+          await this.$store.dispatch('logout');
+          await this.$router.replace('/');
+        } catch (e) {
           console.log(e);
         }
       }
