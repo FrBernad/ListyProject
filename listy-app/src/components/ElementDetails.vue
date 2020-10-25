1f3dae901d81201e2f0b348b939fed075692717b
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2020-10-25 18:11:18 -0300

    Botones y mas

--- listy-app/src/components/ElementDetails.vue
+++ listy-app/src/components/ElementDetails.vue
@@ -28,31 +28,35 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="12">
+      <v-col cols="12" class="pb-0">
         <v-textarea
           outlined
           name="input-7-4"
           v-value="aclaracion"
           label="Aclaración"
           no-resize="true"
+          class="px-1"
         ></v-textarea>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="4" class="d-flex ma-0 pa-0 justify-start">
-        <v-card-text class="px-4">Precio: {{precio}}</v-card-text>
+        <span class="px-4">Precio: {{ precio }}</span>
       </v-col>
     </v-row>
-    <a id="forgot" :class="{'text-decoration-underline':hover}" class="text-center">¿No encontraste el producto que buscabas?</a>
-    <v-card-actions class="justify-end">
-      <v-btn
-        color="#212529"
-        outlined
-        @click="elementClose"
-      >
-        Close
-      </v-btn>
-    </v-card-actions>
+    <v-row>
+      <v-col cols="12">
+        <a id="forgot" :class="{'text-decoration-underline':hover}" class="text-center">¿No encontraste el producto que
+          buscabas?</a>
+      </v-col>
+    </v-row>
+    <v-row class="align-center justify-end">
+      <v-col cols="12" sm="8" md="6" class="d-flex align-center justify-space-around">
+        <v-btn color="#212529" outlined @click="elementClose">Cerrar</v-btn>
+        <v-btn color="#212529" outlined @click="elementClose">Agregar</v-btn>
+      </v-col>
+    </v-row>
+    </v-row>
   </v-card>
 </template>
 
@@ -77,5 +81,7 @@ export default {
 </script>
 
 <style scoped>
-
+#forgot {
+  color: #1D50AE;
+}
 </style>
