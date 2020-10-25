22763a41230a8b489855d6e2b35f7e707d0b8b0c
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2020-10-25 16:15:05 -0300

    List changes

--- listy-app/src/components/ListFooter.vue
+++ listy-app/src/components/ListFooter.vue
@@ -1,16 +1,16 @@
 <template>
   <v-card class="px-5 " elevation="10" outlined>
     <v-row>
-      <v-col cols="1">
+      <v-col cols="6" class="px-5">
         <v-icon color="black">mdi-cart</v-icon>
       </v-col>
-      <v-col cols="5">
-        <span>Total: {{total}} </span>
-      </v-col>
-
-      <v-col cols="6">
+      <v-col cols="3">
         <span>Fondos: {{founds}}</span>
       </v-col>
+      <v-col cols="3">
+        <span class="px-12">Total: {{total}} </span>
+      </v-col>
+
     </v-row>
   </v-card>
 </template>
--- listy-app/src/components/ListItem.vue
+++ listy-app/src/components/ListItem.vue
@@ -6,38 +6,31 @@
         <v-col cols="1">
           <v-checkbox color="#0099FF"></v-checkbox>
         </v-col>
-        <v-col cols="10" class="text-start">
+        <v-col cols="7" class="text-start">
           <span>{{ itemName }}</span>
         </v-col>
-        <v-col cols="1">
+        <v-col cols="2">
           <span>{{ quantity }}</span>
+        </v-col>
+        <v-col cols="2">
+          <span>${{ price }}</span>
         </v-col>
       </v-row>
     </v-expansion-panel-header>
 
     <v-expansion-panel-content>
 
-      <v-row class="align-center  justify-center">
-        <v-col cols="4">
-          <v-text-field background-color="#ffffff" dense hide-details solo label="Cantidad"
-                        clearable>{{ quantity }}</v-text-field>
-        </v-col>
-        <v-col cols="4">
-          <v-text-field background-color="#ffffff" dense hide-details solo label="Precio"
-                        clearable>{{ price }}
-          </v-text-field>
-        </v-col>
-        <v-col cols="4">
-          <v-text-field background-color="#ffffff" dense hide-details solo label="Responsable"
-                        clearable>{{ responsible }}
-          </v-text-field>
-        </v-col>
-        <v-col cols="12">
+      <v-row>
+        <v-col cols="6" class="justify-end">
           <v-text-field background-color="#ffffff" dense hide-details solo label="Aclaración"
                         clearable>{{ aclaracion }}
           </v-text-field>
         </v-col>
-
+        <v-col cols="6" class="justify-start">
+          <v-text-field background-color="#ffffff" dense hide-details solo label="Responsable"
+                        clearable>{{ responsible }}
+          </v-text-field>
+        </v-col>
       </v-row>
 
     </v-expansion-panel-content>
