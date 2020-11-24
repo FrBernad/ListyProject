160ac715301fa72a6bdae2c67441afda8cdf2a1a
Nicolás Rampoldi <nrampoldi@itba.edu.ar>
2020-11-24 10:49:52 -0300

    fix allignment

--- listy-app/src/components/ListItem.vue
+++ listy-app/src/components/ListItem.vue
@@ -2,7 +2,7 @@
   <v-expansion-panel>
 
     <v-expansion-panel-header class="pt-0 pb-0 ">
-      <v-row class="align-center  justify-center">
+      <v-row class="align-center justify-center">
         <v-col cols="1">
           <v-btn @click.stop="" v-if="editable" icon color="#000000" @click="sendDelete">
             <v-icon>mdi-delete</v-icon>
@@ -23,10 +23,10 @@
 
     <v-expansion-panel-content>
       <v-row>
-        <v-col cols="6" class="justify-end">
+        <v-col cols="6" class="justify-end px-6">
             <v-text-field label="Aclaración" v-model="item.note" :readonly="!editable">{{ item.note }}</v-text-field>
         </v-col>
-        <v-col cols="6" class="justify-start">
+        <v-col cols="6" class="justify-start px-8">
             <v-text-field label="Responsable" v-model="item.responsible" :readonly="!editable">{{ item.responsible}}</v-text-field>
         </v-col>
       </v-row>
